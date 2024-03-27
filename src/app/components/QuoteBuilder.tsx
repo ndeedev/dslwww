5ade4308df316f88db713b2bad1b24eb110c5152
Nick Dearborn <nick.dearborn@gmail.com>
2024-03-27 15:37:05 -0700

    component update

--- src/app/components/QuoteBuilder.tsx
+++ src/app/components/QuoteBuilder.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useState } from 'react';
 
 interface Product {
@@ -12,6 +14,7 @@ const QuoteBuilder: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+  const [selectedDiscount, setSelectedDiscount] = useState<number>(1); // Default discount is 100% (no discount)
 
   const fetchProduct = async () => {
     setLoading(true);
@@ -19,7 +22,18 @@ const QuoteBuilder: React.FC = () => {
 
     try {
       const encodedProdNum = encodeURIComponent(prodNum);
-      const response = await fetch(`https://dslwww.vercel.app/api/product/${encodedProdNum}`);
+
+      /*
+        Use this spaghetti to get and use my current URL
+      */
+        const protocol = window.location.protocol; // e.g., "https:"
+        const hostname = window.location.hostname;
+        const port = window.location.port; // Might be an empty string
+      
+        const domain = port ? `${hostname}:${port}` : hostname;
+        const fullUrl = `${protocol}//${domain}`;
+
+      const response = await fetch(`${fullUrl}/api/product/${encodedProdNum}`);
       if (!response.ok) {
         throw new Error('Failed to fetch the product');
       }
@@ -29,6 +43,7 @@ const QuoteBuilder: React.FC = () => {
       }
       setProducts(prev => [...prev, ...data]);
       setProdNum('');
+      setError(null); // Reset error when product is found
     } catch (err: any) {
       setError(err.message);
     } finally {
@@ -36,30 +51,52 @@ const QuoteBuilder: React.FC = () => {
     }
   };
 
-  const removeProduct = (productId: string) => {
-    setProducts(prevProducts => prevProducts.filter(product => product._id !== productId));
+  const removeProduct = (index: number) => {
+    setProducts(prevProducts => {
+      const newProducts = [...prevProducts];
+      newProducts.splice(index, 1);
+      return newProducts;
+    });
+  };
+
+  const getTotalPrice = () => {
+    return products.reduce((total, product) => {
+      const price = isNaN(Number(product.prodListPrice)) ? 0 : Number(product.prodListPrice);
+      return total + (price * selectedDiscount); // Use the discounted price for calculation
+    }, 0);
+  };
+
+  const handleDiscountChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedDiscount(parseFloat(e.target.value));
   };
 
   return (
     <div className="container mx-auto px-4 py-8 my-4 bg-white shadow-lg rounded-lg">
-      <div className="flex justify-end items-center mb-4">
-        <div className="flex gap-4 items-center">
-          {error && (
-            <p className="text-red-500 mr-4">{error}</p>
-          )}
-          <input            
+      <div className="flex justify-end mb-4">
+        <div className="flex items-center">
+          <select
+            className="px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-black"
+            defaultValue="1" // Default value is "List"
+            onChange={handleDiscountChange} // Call handleDiscountChange when the select value changes
+          >
+            <option value="1">List</option>
+            <option value=".6">40%</option>
+            <option value=".5">50%</option>
+            <option value=".4">60%</option>
+          </select>
+          <input
             type="text"
             value={prodNum}
             onChange={(e) => setProdNum(e.target.value)}
             placeholder="Product Number"
-            className={`max-w-[17ch] px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-black ${
-              error ? 'border-red-500' : 'border-black'
+            className={`max-w-[17ch] px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent ${
+              error ? 'text-red-500 border-red-500' : 'text-black border-black'
             }`}
           />
           <button
             onClick={fetchProduct}
             disabled={loading}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           >
             Fetch
           </button>
@@ -70,21 +107,22 @@ const QuoteBuilder: React.FC = () => {
 
       <div className="mt-6">
         <div className="flex text-center text-white bg-blue-500 px-4 py-2 rounded-t-md">
-          <div className="w-1/4 font-bold">Product Number</div>
-          <div className="w-1/2 font-bold">Description</div>
-          <div className="w-1/4 font-bold">List Price</div>
+          <div className="w-1/5 font-bold">Product Number</div>
+          <div className="flex-1 font-bold">Description</div>
+          <div className="w-1/5 font-bold">List Price</div>
+          <div className="w-6 font-bold"></div>
         </div>
 
         {products.map((product, index) => (
-          <div key={product._id} className={`flex text-center px-4 py-2 bg-white text-black border-b border-black`}>
-            <div className="w-1/4">{product.prodNumber}</div>
-            <div className="w-1/2">{product.prodDescription}</div>
-            <div className="w-1/4">
-              ${isNaN(Number(product.prodListPrice)) ? "N/A" : Number(product.prodListPrice).toFixed(2)}
+          <div key={`${product.prodNumber}-${index}`} className={`flex text-center px-4 py-2 bg-white text-black border-b border-black`}>
+            <div className="w-1/5">{product.prodNumber}</div>
+            <div className="flex-1">{product.prodDescription}</div>
+            <div className="w-1/5">
+              ${isNaN(Number(product.prodListPrice)) ? "N/A" : (Number(product.prodListPrice) * selectedDiscount).toFixed(2)}
             </div>
-            <div className="w-[2rem]">
+            <div className="w-6">
               <button
-                onClick={() => removeProduct(product._id)}
+                onClick={() => removeProduct(index)}
                 className="text-red-500"
               >
                 &#10060; 
@@ -92,6 +130,13 @@ const QuoteBuilder: React.FC = () => {
             </div>
           </div>
         ))}
+
+        <div className="flex text-center px-4 py-2 bg-white text-black border-b border-black">
+          <div className="w-1/5"></div>
+          <div className="flex-1 font-bold text-right">Total Price:</div>
+          <div className="w-1/5 font-bold">${getTotalPrice().toFixed(2)}</div>
+          <div className="w-6 font-bold"></div>
+        </div>
       </div>
     </div>
   );
