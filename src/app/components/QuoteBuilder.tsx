66b822352bb6dffd4c9a72ed7c9be7938d1225f4
Nick Dearborn <nick.dearborn@gmail.com>
2024-03-26 20:44:00 -0700

    updating fetch url to match vercel

--- src/app/components/QuoteBuilder.tsx
+++ src/app/components/QuoteBuilder.tsx
@@ -19,7 +19,7 @@ const QuoteBuilder: React.FC = () => {
 
     try {
       const encodedProdNum = encodeURIComponent(prodNum);
-      const response = await fetch(`http://localhost:3000/api/product/${encodedProdNum}`);
+      const response = await fetch(`https://dslwww-7uchinsig-nicks-projects-bd8df28c.vercel.app/${encodedProdNum}`);
       if (!response.ok) {
         throw new Error('Failed to fetch the product');
       }
